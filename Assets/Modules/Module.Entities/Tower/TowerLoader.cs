4eaed00e592b42f57286005eb376c70b040415b2
napoleloc <napoleloc@gmail.com>
2025-01-07 13:00:09 +0700

    🚀 refactor(TowerLoader.cs): Update message handling methods for PubSub integration.

--- Assets/Modules/Module.Entities/Tower/TowerLoader.cs
+++ Assets/Modules/Module.Entities/Tower/TowerLoader.cs
@@ -2,8 +2,11 @@ using System.Runtime.CompilerServices;
 using Cysharp.Threading.Tasks;
 using EncosyTower.Modules;
 using EncosyTower.Modules.AddressableKeys;
+using EncosyTower.Modules.PubSub;
 using EncosyTower.Modules.Vaults;
+using Module.Core.Extended.PubSub;
 using Module.Entities.Tower.Data;
+using Module.Entities.Tower.PubSub;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -27,6 +30,11 @@ namespace Module.Entities.Tower
 
         private void Awake()
         {
+            var subscriber = WorldMessenger.Subscriber.TowerScope().WithState(this);
+            subscriber.Subscribe<AsyncMessage<LoadTowerMessage>>(static (state, msg) => state.HandleAsync(msg));
+            subscriber.Subscribe<LoadTowerMessage>(static (state, msg) => state.Handle(msg));
+            subscriber.Subscribe<UnloadTowerMessage>(static (state, msg) => state.Handle(msg));
+
             GlobalObjectVault.TryAdd(PresetId, this);
             GlobalValueVault<bool>.TrySet(PresetId, true);
         }
@@ -37,29 +45,43 @@ namespace Module.Entities.Tower
             GlobalObjectVault.TryRemove(PresetId, out _);
         }
 
-        public void Load()
+        private async UniTask HandleAsync(AsyncMessage<LoadTowerMessage> asyncMessage)
+        {
+            var message = asyncMessage.Message;
+            await LoadAsyncInternal(message.Id);
+        }
+
+        private void Handle(LoadTowerMessage message)
         {
 
         }
 
-        public void Unload()
+        private void Handle(UnloadTowerMessage message)
         {
-            if (_usedObject.IsValid())
-            {
-                Addressables.ReleaseInstance(_usedObject);
-            }
+
         }
+
+        private async UniTaskVoid LoadAndForget(ushort id)
+            => await LoadAsyncInternal(id);
 
         private async UniTask LoadAsyncInternal(ushort id)
         {
             if (_loaded)
             {
-                Unload();
+                UnloadInternal();
             }
 
             var towerHandle = new AddressableKey<GameObject>(TowerNames.Format(id));
 
             _usedObject = await towerHandle.InstantiateAsync(gameObject.scene, trimCloneSuffix: true);
+        }
+
+        private void UnloadInternal()
+        {
+            if (_usedObject.IsValid())
+            {
+                Addressables.ReleaseInstance(_usedObject);
+            }
         }
     }
 }
