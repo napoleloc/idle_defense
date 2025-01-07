61423ffd33a22ac2a420666f8ee839c47ae48584
napoleloc <napoleloc@gmail.com>
2025-01-07 12:57:53 +0700

    🚀 feat(Tower): Implement initialize, deinitialize method for TowerAttributeComponent and TowerUpgradeComponent

--- Assets/Modules/Module.Entities/Tower/Components/TowerAttributeComponent.cs
+++ Assets/Modules/Module.Entities/Tower/Components/TowerAttributeComponent.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 namespace Module.Entities.Tower
 {
-    public class TowerAttributeComponent : MonoBehaviour
+    public class TowerAttributeComponent : MonoBehaviour, IInitialization, IDeinitialization
     {
+        public void Initialize()
+        {
+            
+        }
+
+        public void Deinitialize()
+        {
+
+        }
     }
 }
