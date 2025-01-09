70381be048170167c8c5368b03bcf70452f1e14f
napoleloc <napoleloc@gmail.com>
2025-01-09 09:38:41 +0700

    Update TowerNames prefab format to include 'building' prefix

--- Assets/Modules/Module.Entities/Tower/Common/TowerNames.cs
+++ Assets/Modules/Module.Entities/Tower/Common/TowerNames.cs
@@ -2,7 +2,7 @@ namespace Module.Entities.Tower
 {
     public static class TowerNames
     {
-        public const string PREFAB_FORMAT = "prefab-tower-{0}";
+        public const string PREFAB_FORMAT = "prefab-building-tower-{0}";
 
         public static string Format(ushort id)
             => string.Format(PREFAB_FORMAT, id);
