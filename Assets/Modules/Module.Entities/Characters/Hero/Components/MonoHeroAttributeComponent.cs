2ed9703a0cb9d2890a9c061dbf5411c2a17a0af9
napoleloc <napoleloc@gmail.com>
2025-01-10 15:02:20 +0700

    Add attack interval properties to MonoHeroAttributeComponent

--- Assets/Modules/Module.Entities/Characters/Hero/Components/MonoHeroAttributeComponent.cs
+++ Assets/Modules/Module.Entities/Characters/Hero/Components/MonoHeroAttributeComponent.cs
@@ -4,5 +4,12 @@ namespace Module.Entities.Characters.Hero
 {
     public class MonoHeroAttributeComponent : MonoBehaviour
     {
+        [SerializeField]
+        private float _normalAttackInterval;
+        [SerializeField]
+        private float _specialAttackInterval;
+
+        public float NormalAttackInterval => _normalAttackInterval;
+        public float SpecialAttackInterval => _specialAttackInterval;
     }
 }
