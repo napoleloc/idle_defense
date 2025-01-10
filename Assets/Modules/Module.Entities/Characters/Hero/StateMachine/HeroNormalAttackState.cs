d9c45da3058a4d9f30b1b8ce1ef55f4acf8de95d
napoleloc <napoleloc@gmail.com>
2025-01-10 15:55:01 +0700

    Added logic to trigger enemy dying animation during normal attack.

--- Assets/Modules/Module.Entities/Characters/Hero/StateMachine/HeroNormalAttackState.cs
+++ Assets/Modules/Module.Entities/Characters/Hero/StateMachine/HeroNormalAttackState.cs
@@ -1,31 +1,48 @@
+using Module.GameCommon.Animation;
 using JetBrains.Annotations;
 
 namespace Module.Entities.Characters.Hero.StateMachine
 {
     public class HeroNormalAttackState : HeroBaseState
     {
+        private readonly MonoHeroTargetFindingComponent _targetFindingComponent;
+        private bool _isAttacking = false;
+
         public HeroNormalAttackState([NotNull] MonoHeroController controller, bool needsExitTime) : base(controller, needsExitTime)
         {
-
+            _targetFindingComponent = controller.TargetFindingComponent;
         }
 
         public override void OnEnter()
         {
             base.OnEnter();
 
-            CharacterAnimationComponent.CrossFadeAnim(GameCommon.Animation.CharAnim.NormalAttack);
+            CharacterAnimationComponent.CrossFadeAnim(CharAnim.NormalAttack);
         }
 
         public override void OnLogic()
         {
-            if(CharacterAnimationComponent.TryGetAnimatorStateInfo(GameCommon.Animation.CharAnim.NormalAttack, ref animatorStateInfo))
+            if(CharacterAnimationComponent.TryGetAnimatorStateInfo(CharAnim.NormalAttack, ref animatorStateInfo))
             {
                 float currentTime = animatorStateInfo.normalizedTime % 1;
+
+                if(_isAttacking == false  
+                    && CharacterAnimationComponent.CanTriggerEvent(CharAnim.NormalAttack, currentTime))
+                {
+                    _targetFindingComponent.TargetEnemy.TriggerDying();
+                    _isAttacking = true;
+                }
+
                 if (currentTime >= 0.99F)
                 {
                     StateMachine.StateCanExit();
                 }
             }
+        }
+
+        public override void OnExit()
+        {
+            _isAttacking = false;
         }
     }
 }
