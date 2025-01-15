e86c7bb52c4694a5a72979801b4c4f999509ac3c
napoleloc <napoleloc@gmail.com>
2025-01-15 09:15:22 +0700

    Update MonoHeroAttributeComponent to use AttributeSystem efficiently

--- Assets/Modules/Module.Entities/Characters/Hero/Components/MonoHeroAttributeComponent.cs
+++ Assets/Modules/Module.Entities/Characters/Hero/Components/MonoHeroAttributeComponent.cs
@@ -1,15 +1,28 @@
+using System.Runtime.CompilerServices;
+using Module.Worlds.BattleWorld.Attribute;
 using UnityEngine;
 
 namespace Module.Entities.Characters.Hero
 {
-    public class MonoHeroAttributeComponent : MonoBehaviour
+    public class MonoHeroAttributeComponent : MonoBehaviour, IEntityComponent, IDeinitialization
     {
-        [SerializeField]
-        private float _normalAttackInterval;
-        [SerializeField]
-        private float _specialAttackInterval;
+        private AttributeSystem _attributeSystem;
 
-        public float NormalAttackInterval => _normalAttackInterval;
-        public float SpecialAttackInterval => _specialAttackInterval;
+        public void InitializeDependencies()
+        {
+            _attributeSystem = new();
+        }
+
+        public void Deinitialize()
+        {
+            _attributeSystem.Dispose();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public float GetValue(AttributeType type)
+        {
+            var result = _attributeSystem.TryGet(type, out var attribute);
+            return result ? attribute.Value : 0;
+        }
     }
 }
