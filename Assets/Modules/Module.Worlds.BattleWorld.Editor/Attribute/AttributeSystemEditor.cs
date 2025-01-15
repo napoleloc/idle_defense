7280cc9bb354bf2acd23e3848732e4e9206215f1
napoleloc <napoleloc@gmail.com>
2025-01-15 09:09:07 +0700

    Prepare editor tooling for Attribute

---
+++ Assets/Modules/Module.Worlds.BattleWorld.Editor/Attribute/AttributeSystemEditor.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Module.Worlds.BattleWorld.Editor.Attribue
+{
+    using Editor = UnityEditor.Editor;
+
+    
+    public class AttributeSystemEditor : Editor
+    {
+        
+    }
+}
