bc46a529d0431c84bb61f76e4bf50264cb566d5d
napoleloc <napoleloc@gmail.com>
2025-01-27 07:49:22 +0700

    Implement SmoothChangeVolume method for AudioChannel

--- Assets/Modules/Module.Core.Extended/Audio/AudioChannel.cs
+++ Assets/Modules/Module.Core.Extended/Audio/AudioChannel.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
-using Module.Core.Extended.Audio.Interfaces;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -7,36 +9,80 @@ namespace Module.Core.Extended.Audio
 {
     public class AudioChannel : MonoBehaviour, IAudioChannel, IAudioPoolable
     {
-        protected AudioSource source;
+        protected bool looping;
+        protected bool paused;
+        protected float volume;
+        protected AudioSource audioSource;
+        protected AsyncLazy asyncLazy;
+
+        public void Test()
+        {
+            var task = SmoothChangeVolume(0, 0, default);
+            if(task.Status == UniTaskStatus.Succeeded)
+            {
+
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected bool Validate()
+           => audioSource && audioSource.clip;
+
+        private async UniTask SmoothChangeVolume(float targetVolume, float duration, CancellationToken token)
+        {
+            while (duration > 0.01F)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                volume = Mathf.Lerp(volume, targetVolume, Time.deltaTime / duration);
+                await UniTask.WaitForEndOfFrame(token);
+                duration -= Time.deltaTime;
+
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+
+            if(token.IsCancellationRequested)
+            {
+                return;
+            }
+
+            volume = targetVolume;
+        }
 
         public void Initialize([NotNull] AudioClip clip)
         {
-            source.clip = clip;
+            audioSource.clip = clip;
         }
 
         public void SetAudioMixerGroup(AudioMixerGroup mixerGroup)
         {
-            
+
         }
 
         public void SetLoop(bool loop)
         {
-            
+
         }
 
         public void SetVolume(float volume)
         {
-            
+
         }
 
         public virtual void OnGetFromPool()
         {
-            
+
         }
 
         public virtual void OnReturnToPool()
         {
-            
+
         }
     }
 }
